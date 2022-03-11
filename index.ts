7e0d6c088cd0f8010a223fcda3f8930820741373
Herman <herman@q42.nl>
2022-03-11 17:00:14 +0100

    wip

---
+++ index.ts
@@ -0,0 +1,22 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import * as fs from "fs";
+import * as glob from "glob";
+
+try {
+  glob(core.getInput("sourcePaths", { required: true }))
+  bucket;
+  destinationPath;
+  sourcePath;
+  html;
+  // `who-to-greet` input defined in action metadata file
+  const nameToGreet = core.getInput("who-to-greet");
+  console.log(`Hello ${nameToGreet}!`);
+  const time = new Date().toTimeString();
+  core.setOutput("time", time);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
